13c1dab01808f0e241c3e6e6783e691a93c6a43f
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2018-06-11 18:09:16 +0200

    Add files via upload

--- Nickwasuseds Proxy Tool.py
+++ Nickwasuseds Proxy Tool.py
@@ -2,19 +2,27 @@ import requests
 import time
 import os
 from os import getcwd
+
 print('Made by Nickwasused')
 print('*******************')
-print('Version: 0.2')
+print('Version: 0.4')
 print('Sources for Proxy Servers:')
 print('https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt')
 print('https://raw.githubusercontent.com/a2u/free-proxy-list/master/free-proxy-list.txt')
+print('https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list')
+print('https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt')
+os.system('fsutil file createnew proxylist.txt 1')
 os.remove("proxylist.txt")
 url1 = "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt"
 url2 = "https://raw.githubusercontent.com/a2u/free-proxy-list/master/free-proxy-list.txt"
+url3 = "https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list"
+url4 = "https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt"
 directory = getcwd()
 filename = directory + 'proxylist.txt'
 r = requests.get(url1)
 t = requests.get(url2)
+l = requests.get(url3)
+s = requests.get(url4)
 with open('proxylist.txt', 'a') as the_file:
     the_file.write('***********************************************')
 with open('proxylist.txt', 'a') as the_file:
@@ -26,8 +34,20 @@ with open('proxylist.txt', 'a') as the_file:
 with open('proxylist.txt', 'a') as the_file:
     the_file.write(t.text)
 with open('proxylist.txt', 'a') as the_file:
-    the_file.write('Sources:https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt,')
+    the_file.write(l.text)
 with open('proxylist.txt', 'a') as the_file:
-    the_file.write('https://raw.githubusercontent.com/a2u/free-proxy-list/master/free-proxy-list.txt')
-time.sleep(30)
+    the_file.write(s.text)
+with open('proxylist.txt', 'a') as the_file:
+    the_file.write(url1)
+with open('proxylist.txt', 'a') as the_file:
+    the_file.write(url2)
+with open('proxylist.txt', 'a') as the_file:
+    the_file.write(url3)
+with open('proxylist.txt', 'a') as the_file:
+    the_file.write(url4)
+print('got this:')
+print(r.text)
+print(t.text)
+print(l.text)
+print(s.text)
 
