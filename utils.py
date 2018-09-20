600a78d47040a5c58cd20439206c991a2216fb8a
Nickwasused <1771@gmx.de>
2018-09-20 13:40:33 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 import requests
 import time
 import os
+import variables
 import urllib
 import sys
 import socket
+import random
 import info
 import utils
 import urllib.request
-from os import getcwd  # done
+from os import getcwd
+from os import system as term
 from pathlib import Path
 from tqdm import tqdm
 
+var = variables.urls()
 
 def checkfilewindows():
     my_file = Path("proxylist.txt")
@@ -44,11 +48,14 @@ def countproxys():
     num_lines = sum(1 for line in open('proxylist.txt'))  # reading the File Lines
     print('Got {} Proxyies!'.format(num_lines))  # and Again....
     print("Sleeping 5 Secconds")
+    print(" ")
+    print(" ")
+    print(" ")
 
 
 def downloadproxy1():
     chunk_size = 1024
-    url1 = "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt"  # url 1-4
+    url1 = var.url1
     a = requests.get(url1, stream=True)
     total_size_1 = int(a.headers['content-length'])
     with open('proxylist.txt', 'wb') as f:
@@ -59,7 +66,7 @@ def downloadproxy1():
 
 def downloadproxy2():
     chunk_size = 1024
-    url2 = "https://raw.githubusercontent.com/a2u/free-proxy-list/master/free-proxy-list.txt"
+    url2 = var.url2
     b = requests.get(url2, stream=True)
     total_size_2 = int(b.headers['content-length'])
     with open('proxylist.txt', 'wb') as f:
@@ -70,7 +77,7 @@ def downloadproxy2():
 
 def downloadproxy3():
     chunk_size = 1024
-    url3 = "https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt"
+    url3 = var.url3
     c = requests.get(url3, stream=True)
     total_size_3 = int(c.headers['content-length'])
     with open('proxylist.txt', 'wb') as f:
@@ -81,7 +88,7 @@ def downloadproxy3():
 
 def downloadproxy4():
     chunk_size = 1024
-    url4 = "https://raw.githubusercontent.com/DarrenRainey/HTTP-Proxy-List/master/lists/1025674"
+    url4 = var.url4
     d = requests.get(url4, stream=True)
     total_size_4 = int(d.headers['content-length'])
     with open('proxylist.txt', 'wb') as f:
@@ -93,7 +100,6 @@ def downloadproxy4():
 def checkproxys():
     from checker import checker
     from colorama import Fore
-    from os import system as term
 
     phile = 'proxylist.txt'
     filer = open(phile)
@@ -101,8 +107,16 @@ def checkproxys():
     checker = checker()
     for item in filer:
         if checker.check(item):
-            print(Fore.RED + 'Bad proxy', item)
+            temp1 = random.randint(1, 4)
+            if temp1 == 1:
+                print(Fore.RED + 'Bad proxy', item)
+            if temp1 == 2:
+                print(Fore.BLUE + 'Bad proxy', item)
+            if temp1 == 3:
+                print(Fore.YELLOW + 'Bad proxy', item)
+            if temp1 == 4:
+                print(Fore.CYAN + 'Bad proxy', item)
         else:
             phile = open('working.txt', 'a')
             phile.write(item)
-    print(Fore.GREEN + 'Good proxy', item)
+            print(Fore.GREEN + 'Good proxy', item)
